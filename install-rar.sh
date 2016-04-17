5794069c5958bdfc0004b8895040306ec8e6b80e
sayem314 <sayem314@gmail.com>
2016-04-17 13:21:22 +0600

    Create install-rar.sh
    First commit

---
+++ install-rar.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Easy Winrar for Linux
+# v1.0
+#
+
+	#Detect architecture
+	if [ -n "$(uname -m | grep 64)" ]; then
+		echo "  Downloading Winrar 5.3.0 for 64bit"
+		winrar="rarlinux-x64-5.3.0.tar.gz"
+	elif [ -n "$(uname -m | grep 86)" ]; then
+		echo "  Downloading Winrar 5.3.0 for 32bit"
+		winrar="rarlinux-5.3.0.tar.gz"
+	else
+		echo "  unsupported or unknown architecture"
+		echo ""
+		exit;
+	fi
+	cd /tmp
+	wget http://rarlab.com/rar/$winrar
+	tar xzf rarlinux*
+	cd rar
+	PREFIX=/usr/local
+	mkdir -p $(PREFIX)/bin
+	mkdir -p $(PREFIX)/lib
+	#Deleting any previous version
+	rm -f $(PREFIX)/bin/rar
+	rm -f $(PREFIX)/bin/unrar
+	rm -f /etc/rarfiles.lst
+	rm -f $(PREFIX)/lib/default.sfx	
+	#Copying rar unrar
+	cp rar unrar $(PREFIX)/bin
+	cp rarfiles.lst /etc
+	cp default.sfx $(PREFIX)/lib
+	cd /tmp
+	rm -rf rar
+	rm -f rarlinux*
+	echo "  Done :)"
+	exit;
